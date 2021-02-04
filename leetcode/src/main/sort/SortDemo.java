0e9238e0ca4e77218671dfc652a04abaae5cbd0f
IamGoodGood <48451211@qq.com>
2021-02-05 00:38:02 +0800

    2020-2-5

--- leetcode/src/main/sort/SortDemo.java
+++ leetcode/src/main/sort/SortDemo.java
@@ -52,13 +52,53 @@ public class SortDemo {
     }
 
     // 快速排序
+    public void quickSort(int[] arr) {
+        quickSort(arr, 0, arr.length - 1);
+    }
+
+    private void quickSort(int[] arr, int leftIndex, int rightIndex) {
+        if (leftIndex >= rightIndex) {
+            return;
+        }
+
+        int left = leftIndex;
+        int right = rightIndex;
+        //待排序的第一个元素作为基准值
+        int key = arr[left];
+
+        //从左右两边交替扫描，直到left = right
+        while (left < right) {
+            while (right > left && arr[right] >= key) {
+                //从右往左扫描，找到第一个比基准值小的元素
+                right--;
+            }
+
+            //找到这种元素将arr[right]放入arr[left]中
+            arr[left] = arr[right];
+
+            while (left < right && arr[left] <= key) {
+                //从左往右扫描，找到第一个比基准值大的元素
+                left++;
+            }
+
+            //找到这种元素将arr[left]放入arr[right]中
+            arr[right] = arr[left];
+        }
+        //基准值归位
+        arr[left] = key;
+        //对基准值左边的元素进行递归排序
+        quickSort(arr, leftIndex, left - 1);
+        //对基准值右边的元素进行递归排序。
+        quickSort(arr, right + 1, rightIndex);
+    }
 
     public static void main(String[] args) {
         int[] arr = new int[]{3, 1, 10, 17, 22, 15, 5, 9};
         SortDemo demo = new SortDemo();
 //        demo.bubbleSort(arr);
 //        demo.selectionSort(arr);
-        demo.insertionSort(arr);
+//        demo.insertionSort(arr);
+        demo.quickSort(arr);
         for (int i : arr) {
             System.out.print(i + " ");
         }
