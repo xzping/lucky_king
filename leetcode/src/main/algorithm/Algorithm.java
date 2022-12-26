d615354f68d87f2d5c1a8e42713a1179b3d882fc
xiezhiping <zhiping.xie@dji.com>
2022-12-26 18:37:58 +0800

    2022-12-26

--- leetcode/src/main/algorithm/Algorithm.java
+++ leetcode/src/main/algorithm/Algorithm.java
@@ -698,6 +698,37 @@ public class Algorithm {
         return ans;
     }
 
+    /**
+     * 最大子数组和
+     * <p>
+     * dp[i]：表示以nums[i]结尾的连续子数组的最大和
+     * dp[i] = dp[i-1] + nums[i] , dp[i-1]>0
+     * dp[i] = nums[i], dp[i-1]<=0
+     * dp[0] = nums[0]
+     *
+     * @param nums
+     * @return
+     */
+    public int maxSubArray(int[] nums) {
+        int len = nums.length;
+        int[] dp = new int[len];
+        dp[0] = nums[0]; // 初始值
+
+        for (int i = 1; i < len; i++) {
+            if (dp[i - 1] > 0) {
+                dp[i] = dp[i - 1] + nums[i];
+            } else {
+                dp[i] = nums[i];
+            }
+        }
+
+        int max = dp[0];
+        for (int i = 1; i < dp.length; i++) {
+            max = Math.max(max, dp[i]);
+        }
+        return max;
+    }
+
     public static void main(String[] args) {
         String s = "abcabcbb";
         Algorithm algorithm = new Algorithm();
