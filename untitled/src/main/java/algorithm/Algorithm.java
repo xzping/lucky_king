aa407489814f119c1c19698cca77ae776e2b24ff
zhiping.xie@dji.com <zhiping.xie@dji.com>
2025-01-08 17:13:19 +0800

    20250108

--- untitled/src/main/java/algorithm/Algorithm.java
+++ untitled/src/main/java/algorithm/Algorithm.java
@@ -1066,10 +1066,27 @@ public class Algorithm {
         }
     }
 
-    public static void main(String[] args) {
-        Algorithm algorithm = new Algorithm();
-        int[] nums = {1, 2, 3};
-        algorithm.permute(nums);
+    // 接雨水
+    public int trap(int[] height) {
+        int n = height.length;
+        if (n == 0) {
+            return 0;
+        }
+        int[] leftMax = new int[n];
+        leftMax[0] = height[0];
+        for (int i = 1; i < n; i++) {
+            leftMax[i] = Math.max(leftMax[i - 1], height[i]);
+        }
+        int[] rightMax = new int[n];
+        rightMax[n - 1] = height[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            rightMax[i] = Math.max(rightMax[i + 1], height[i]);
+        }
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            ans += Math.min(leftMax[i], rightMax[i]) - height[i];
+        }
+        return ans;
     }
 }
 
